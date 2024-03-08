26f5bfaafee1e052d8045f6c7299c5e52df65eab
[Diego] <[diegoemanuel134@gmail.com>
2024-03-08 11:18:11 -0600

    Formik Implementado en DialogCategory

--- src/restaurante/categoria/components/DialogCategory.jsx
+++ src/restaurante/categoria/components/DialogCategory.jsx
@@ -1,8 +1,10 @@
-import { Dialog } from "primereact/dialog"
-import { useCategoryStore } from "../../../hooks";
-import { Fragment, useEffect, useState } from "react";
-import { InputText } from "primereact/inputtext";
-import { Button } from "primereact/button";
+import { useFormik } from 'formik';
+import { useCategoryStore } from '../../../hooks';
+import { Fragment, useEffect } from 'react';
+import { Button } from 'primereact/button';
+import { Dialog } from 'primereact/dialog';
+import { InputText } from 'primereact/inputtext';
+import { classNames } from 'primereact/utils';
 
 const category = {
   nombre: '',
@@ -13,67 +15,71 @@ const category = {
 export const DialogCategory = () => {
 
   const { isCategoryModalOpen, closeCategoryModal, activeCategory, startSavingCategory  } = useCategoryStore();
-  
-  const [formSubmitted, setFormSubmitted] = useState(false);
 
-  const [formValues, setFormValues] = useState(category)
-
-  const onInputChange = ({target}) => {
-    setFormValues({
-        ...formValues,
-        [target.name]: target.value
-    })
+  useEffect(() => {
+      if( activeCategory !== null){
+        formik.setValues({...activeCategory});
   }
+  }, [activeCategory])
 
-    useEffect(() => {
-    if( activeCategory !== null){
-      setFormValues({...activeCategory});
-    }
-    }, [activeCategory])
-
-  const onSubmit = async(event)=> {
-    event.preventDefault();
-    setFormSubmitted(true);
-
-    if(formValues.nombre.length <= 0) return;
-    if(formValues.descripcion.length <= 0) return;
-
-    await startSavingCategory( formValues );
-    onCloseDialog();
-    setFormSubmitted(false);
+  const formik = useFormik({
+      initialValues: category,
+      validate: (data) => {
+          let errors = {};
+          if (!data.nombre) {
+              errors.nombre = 'El nombre es requirido.';
+          }
+          if (!data.descripcion) {
+              errors.descripcion = 'La descripcion es requirida.';
+          }
+          return errors;
+      },
+      onSubmit: (data) => {
+      startSavingCategory(data);
+      onCloseDialog();
+      formik.resetForm();
   }
+  });
+
+  const isFormFieldValid = (name) => !!(formik.touched[name] && formik.errors[name]);
+  const getFormErrorMessage = (name) => {
+      return isFormFieldValid(name) && <small className="p-error">{formik.errors[name]}</small>;
+  };
+
 
   const onCloseDialog = () => {
-    setFormValues(category);
-    closeCategoryModal();
+      closeCategoryModal();
+      formik.resetForm();
+    }
+  
+  
+    const categoryDialogFooter = (
+      <Fragment>
+        <Button label="Cancelar" outlined icon="pi pi-times" onClick={onCloseDialog} />
+        <Button label="Guardar" icon="pi pi-check" onClick={formik.handleSubmit} type="submit" />
+      </Fragment>
+    );
+  
+   
+    return (
+              <Dialog header={activeCategory ? 'Modificar Categoria' : 'Agregar Categoria'} visible={isCategoryModalOpen} onHide={onCloseDialog} draggable={false} footer={categoryDialogFooter}
+                  style={{ width: '32rem' }} breakpoints={{ '960px': '75vw', '641px': '90vw' }} modal className="p-fluid"
+              >
+                   <div className="field">
+                      <label htmlFor="nombre" className={classNames('font-bold', { 'p-error': isFormFieldValid('nombre') })}>
+                          Nombre
+                      </label>
+                      <InputText id="nombre" name="nombre" value={formik.values.nombre} onChange={formik.handleChange} autoFocus className={classNames({ 'p-invalid': isFormFieldValid('nombre') })} />
+                      {getFormErrorMessage('nombre')}
+                  </div>
+                  <div className="field">
+                      <label htmlFor="descripcion" className={classNames('font-bold', { 'p-error': isFormFieldValid('descripcion') })}>
+                          Descripción
+                      </label>
+                      <InputText id="descripcion" name="descripcion" value={formik.values.descripcion} onChange={formik.handleChange} className={classNames({ 'p-invalid': isFormFieldValid('descripcion') })} />
+                      {getFormErrorMessage('descripcion')}
+                  </div>
+              </Dialog>
+    )
   }
-
-
-  const categoryDialogFooter = (
-    <Fragment>
-      <Button label="Cancelar" outlined icon="pi pi-times" onClick={onCloseDialog} />
-      <Button label="Guardar" icon="pi pi-check" onClick={onSubmit}/>
-    </Fragment>
-  );
-
- 
-  return (
-
-            <Dialog header="Agregar Categoria" visible={isCategoryModalOpen} onHide={onCloseDialog} draggable={false} footer={categoryDialogFooter}
-                style={{ width: '32rem' }} breakpoints={{ '960px': '75vw', '641px': '90vw' }} modal className="p-fluid"
-            >
-                 <div className="field">
-                    <label htmlFor="nombre" className="font-bold">
-                        Nombre
-                    </label>
-                    <InputText id="nombre" name="nombre" value={formValues.nombre} onChange={onInputChange} required />
-                </div>
-                <div className="field">
-                    <label htmlFor="descripcion" className="font-bold">
-                        Descripción
-                    </label>
-                    <InputText id="descripcion" name="descripcion" value={formValues.descripcion} onChange={onInputChange} required />
-                </div>
-            </Dialog>
-  )
-}
+  
